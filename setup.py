90b9b4beb57533c1182da16b63f561ead0e26ace
Robin Bryce <robinbryce@gmail.com>
2022-02-05 17:55:07 +0000

    more cleanup

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-setup(
-    name="pytokenator",
-    package_dir={"":"py"},
-    py_modules=["gcpblob", "gcpsecrets", "josekeys"],
-    entry_points=dict(
-        console_scripts=["josekeys=josekeys:run"]
-    )
-)
